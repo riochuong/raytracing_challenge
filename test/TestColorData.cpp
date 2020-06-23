0213c336de50d24b628988ab160a0f9836b74f90
chuongdao <cd.kernel.dev.105@gmail.com>
2020-06-23 02:23:27 -0700

    finish creating color"

--- test/TestColorData.cpp
+++ test/TestColorData.cpp
@@ -13,4 +13,31 @@ TEST_CASE("Adding Colors") {
     REQUIRE((c3.B - 1.0f) < 0.00001);
 }
 
+TEST_CASE("Subtracting Colors") {
+    Color c1(0.9, 0.6, 0.75);
+    Color c2(0.7, 0.1, 0.25);
+    Color c3 = c1 - c2;
+    REQUIRE((c3.R - 0.2f) < 0.00001);
+    REQUIRE((c3.G - 0.5f) < 0.00001);
+    REQUIRE((c3.B - 0.5f) < 0.00001);
+}
+
+TEST_CASE("Multiplying Colors") {
+    Color c1(0.2, 0.3, 0.4);
+    Color c2(0.4, 0.6, 0.8);
+    Color c3 = c1 * c2;
+    REQUIRE((c3.R - 0.8f) < 0.00001);
+    REQUIRE((c3.G - 0.18f) < 0.00001);
+    REQUIRE((c3.B - 0.32f) < 0.00001);
+}
+
+TEST_CASE("Multiplying Scalars") {
+    Color c1(0.9, 0.6, 0.75);
+    double scalar = 2.0;
+    Color c3 = c1 * scalar;
+    REQUIRE((c3.R - 1.8f) < 0.00001);
+    REQUIRE((c3.G - 1.2f) < 0.00001);
+    REQUIRE((c3.B - 1.5f) < 0.00001);
+}
+
 
