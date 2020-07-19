12c73044825be2852e30a4866d0ae364b96a3ef2
chuongdao <cd.kernel.dev.105@gmail.com>
2020-07-19 02:14:47 -0700

    add light and shading to app

--- src/raysphereapp.cpp
+++ src/raysphereapp.cpp
@@ -15,10 +15,16 @@ int main (int argc, char** argv){
     // z location of wall 
     double wallZ = 10;
     double wallSize = 7.0;
-    int numCanvasPixels = 400;
+    int numCanvasPixels = 100;
     double pixelSize = wallSize / numCanvasPixels;
     double halfWallSize = wallSize / 2.0;
     Sphere s;
+    s.material = Material();
+    s.material.color = Color(1, 0.2, 1);
+
+    // add light source 
+    PointLight light(Point(20, 0, -10), Color(0.5, 0.5, 0.5));
+
     Color red(1, 0, 0);
     Canvas canvas (numCanvasPixels, numCanvasPixels);
     auto xs = xt::arange(0, numCanvasPixels);
@@ -31,12 +37,23 @@ int main (int argc, char** argv){
         for(int y = 0; y < worldY.shape()[0]; y++){
             Point pos (worldX[x], worldY[y], wallZ);
             Vect vect = pos - rayOrigin;
-            auto sphereTransform = tf::scaling(0.5, 1, 1);
+            auto sphereTransform = tf::scaling(1, 1, 1);
             auto rayTf = tf::transformRay(Ray(rayOrigin, vect.normalize()), 
                                           xt::linalg::inv(sphereTransform));
-            if (hit(intersect(s, rayTf)) >= 0) {
-
-                canvas.writePixel(red, x, y);
+            
+            auto intersects = intersect(s, rayTf);
+            auto hit_idx = hit(intersects);
+            if (hit_idx >= 0) {
+                
+                auto hit_obj = intersects[hit_idx];
+                auto hit_point = position(rayTf, hit_obj.t);
+                auto normal_at_hit_point = hit_obj.object.getNormalAt(hit_point);
+                auto eye_vect = rayTf.vect * (-1);
+                auto color = lighting(hit_obj.object.material, 
+                                      light, hit_point, 
+                                      eye_vect, 
+                                      normal_at_hit_point);
+                canvas.writePixel(color, x, y);
             }
         }
     }
